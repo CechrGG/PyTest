f794ed1da4ea9d640df2406ee988493760f421f1
Leslie_GuoGuo <Leslie_GuoGuo@163.com>
2019-04-30 18:41:14 +0800

    汉诺塔

--- func.py
+++ func.py
@@ -62,5 +62,15 @@ def fact_iter(n, ret):
         return ret
     else:
         return fact_iter(n-1, ret*n)
-print(fact_opti(1000))
+print(fact_opti(10))
 # 遗憾的是python解释器并没有做优化，依然溢出
+
+#汉诺塔
+def hanoi(n, a, b, c):
+    if n == 1:
+        print(a, "-->", c)
+    else :
+        hanoi(n-1, a, c, b)
+        print(a, "-->", c)
+        hanoi(n-1, b, a, c)
+hanoi(3, "A", "B", "C")
